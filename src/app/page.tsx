333e09d1e8d4b9256a5e21c5cb44ff7715d4a8e8
illhrek <109074837+illgreek@users.noreply.github.com>
2024-04-20 19:52:39 +0300

    fix bottom padd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const Home = () => {
     const user = useSelector((state: RootState) => state.user);
 
     return (
-        <main className="flex min-h-screen flex-col items-center px-6">
+        <main className="flex min-h-screen flex-col items-center px-6 pb-6">
             <UserInformation />
             <div className="flex w-full flex-row gap-8 justify-between px-2">
                 <Chart />
